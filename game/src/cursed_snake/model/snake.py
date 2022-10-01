47a000c582f4562489ca2d67308e781045f6d413
Stewart Hutchins <stewartmhutchins@gmail.com>
2022-10-01 15:48:42 +0100

    Fix create_starting_snake function.

--- game/src/cursed_snake/model/snake.py
+++ game/src/cursed_snake/model/snake.py
@@ -1,11 +1,19 @@
-from cursed_snake.model.snake_types import Snake, Coordinate, Direction
+from cursed_snake.model.directions import west
+
+from cursed_snake.model.snake_types import Snake, Coordinate, Direction, Head
+from itertools import accumulate
 
 _STARTING_LENGTH: int = 3
-_STARTING_POSITION: Coordinate = (0, 0)
+_STARTING_HEAD_COORDINATE: Coordinate = (5, 5)
+
+
+def _create_snake(head_coordinate: Coordinate, direction: Direction, length: int) -> Snake:
+    head, *tail = list(accumulate(range(length - 1), lambda acc, _: direction(acc), initial=head_coordinate))
+    return head, tail
 
 
 def create_starting_snake() -> Snake:
-    return [_STARTING_POSITION]
+    return _create_snake(_STARTING_HEAD_COORDINATE, west, _STARTING_LENGTH)
 
 
 def move_snake(snake: Snake, direction: Direction) -> Snake:
