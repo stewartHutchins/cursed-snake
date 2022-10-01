43b51518ecff4ab0a988ed180a9f5b622322e12b
djpiper28 <djpiper28@gmail.com>
2022-10-01 13:25:36 +0100

    feat: added some spaghetti netty code

---
+++ controllers/gyro/android/app/src/main/java/uk/co/djpiper28/gyrosnake/InputFrame.java
@@ -0,0 +1,48 @@
+package uk.co.djpiper28.gyrosnake;
+
+import java.io.IOException;
+import java.util.LinkedList;
+import java.util.List;
+
+public class InputFrame {
+    private boolean xPlus;
+    private boolean xMinus;
+    private boolean yPlus;
+    private boolean yMinus;
+
+    public InputFrame() {
+        this.xPlus = false;
+        this.xMinus = false;
+        this.yPlus = false;
+        this.yMinus = false;
+    }
+
+    public void addInput(MovementTypes type) {
+        switch(type) {
+            case X_PLUS:
+                this.xPlus = true;
+                break;
+            case X_MINUS:
+                this.xMinus = true;
+                break;
+            case Y_PLUS:
+                this.yPlus = true;
+                break;
+            case Y_MINUS:
+                this.yMinus = true;
+                break;
+        }
+    }
+
+    public void transmitFrame(Api api) throws IOException {
+        List<MovementTypes> things = new LinkedList<>();
+        if (this.xPlus) things.add(MovementTypes.X_PLUS);
+        if (this.xMinus) things.add(MovementTypes.X_MINUS);
+        if (this.yPlus) things.add(MovementTypes.Y_PLUS);
+        if (this.yMinus) things.add(MovementTypes.Y_MINUS);
+
+        for (MovementTypes thing : things) {
+            api.sendCommand(thing);
+        }
+    }
+}
--- controllers/gyro/android/app/src/main/java/uk/co/djpiper28/gyrosnake/MainActivity.java
+++ controllers/gyro/android/app/src/main/java/uk/co/djpiper28/gyrosnake/MainActivity.java
@@ -5,6 +5,13 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
+    private FrameHandler frameHandler;
+    private Api api;
+
+    public MainActivity() {
+        this.api = new Api(this.getApplicationContext());
+        this.frameHandler = new FrameHandler(this.api);
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
