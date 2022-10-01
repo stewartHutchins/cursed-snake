04804771c8c0420327e94087fe8ca7cd1dcb0464
djpiper28 <djpiper28@gmail.com>
2022-10-01 19:37:48 +0100

    feat: read da bee movie script for the best typing game ever

--- controllers/typing_game/main.c
+++ controllers/typing_game/main.c
@@ -8,5 +8,11 @@ int main(int argc, char **argv)
 {
     config_t config = get_config();
     int res = transmit_movement(config, X_PLUS);
+
+    for (int i = 0; i < config.words_size; i++) {
+      puts(config.words[i]);
+    }
+
+    free_config(config);
     return res ? 0 : 1;
 }
