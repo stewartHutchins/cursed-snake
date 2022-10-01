4506301dc0808f99583c644f345e8bfb64ad01dd
djpiper28 <djpiper28@gmail.com>
2022-10-01 12:07:35 +0100

    feat: added an android project

---
+++ controllers/gyro/android/app/src/main/java/uk/co/djpiper28/gyrosnake/MainActivity.java
@@ -0,0 +1,14 @@
+package uk.co.djpiper28.gyrosnake;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
