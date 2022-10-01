eb708fc8d2c1007dff8bdf545b9e0841ac9d05e4
Stewart Hutchins <stewartmhutchins@gmail.com>
2022-10-01 14:21:33 +0100

    Create game model.

---
+++ game/src/cursed_snake/model/directions.py
@@ -0,0 +1,22 @@
+from cursed_snake.model.snake_types import Coordinate
+
+
+def _offset(coord: Coordinate, x_offset: int, y_offset: int) -> Coordinate:
+    x, y = coord
+    return x + x_offset, y + y_offset
+
+
+def west(coord: Coordinate) -> Coordinate:
+    return _offset(coord, -1, 0)
+
+
+def east(coord: Coordinate) -> Coordinate:
+    return _offset(coord, +1, 0)
+
+
+def north(coord: Coordinate) -> Coordinate:
+    return _offset(coord, 0, -1)
+
+
+def south(coord: Coordinate) -> Coordinate:
+    return _offset(coord, 0, +1)
